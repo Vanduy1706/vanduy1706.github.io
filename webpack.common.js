9bbb4981d479af9623d66bb69d51002abf7704bf
Vo Van Duy <vovanduy170603@gmail.com>
2025-09-04 10:12:48 +0700

    Fix webpack

--- webpack.common.js
+++ webpack.common.js
@@ -7,7 +7,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     clean: true,
     publicPath: "/",
   },
